eae9f953e68f4eaf746452fe3454641fb00e5821
Giridharan002 <giri.2004k@gmail.com>
2025-03-03 20:20:20 +0530

    first commit

--- cypress/e2e/test-1.cy.js
+++ cypress/e2e/test-1.cy.js
@@ -1,22 +1,19 @@
 describe('Login and Print Last Update and Created by Information', () => {
   it('should log in and print the last update and created by information', () => {
-    // Visit the login page
+    
     cy.visit('https://qa.worktool.kavia.ai/users/login?tenant_id=T0002');
 
-    // Log in
     cy.get('#username').type('derrick@kavia.ai');
     cy.get('[placeholder="Password"]').type('Samuel@1996');
     cy.get('button[type="submit"]').click();
 
-    // Ensure login success
+    
     cy.url({ timeout: 10000 }).should('include', '/dashboard/groups');
 
-    // Navigate to Test App
     cy.contains('Application').click();
     cy.wait(3000);
     cy.contains('Test App').click();
 
-    // Wait for elements to be visible
     cy.wait(5000);
 
     // Extract "Last Update" value
